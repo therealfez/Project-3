6b989b2817c5c983b41f2c2dc36bafd246fb79cc
Daniel Chiu <cycdaniel@gmail.com>
2019-11-10 15:22:31 -0600

    Changed the style of the navbar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,17 +2,23 @@ import React from "react";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        Trippin'
-      </a>
-      <a className="navbar-brand" href="/">
-        Browse
-      </a>
-      <a className="navbar-brand" href="/share">
-        Share
-      </a>
+    <nav className="navbar navbar-expand-lg navbar-light" style={{backgroundColor: "#e3f2fd"}}>
+      <a className="navbar-brand" href="/">Trippin'</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li className="nav-item active">
+              <a className="nav-link" href="/">Search<span class="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">Upload</a>
+            </li>
+          </ul>
+        </div>
     </nav>
+
   );
 }
 
